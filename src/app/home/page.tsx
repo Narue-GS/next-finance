c8acbd8fe3042f4d1eb23bc768f59a969825fdf1
Naruê Gonçalves Santos <naruegit@gmail.com>
2023-05-25 13:44:05 -0300

    fix: fix unstable feature

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,25 +5,15 @@ import Exemplo  from './components/exemplo'
 import { UserContext } from '../hooks/useUser'
 import { useContext } from 'react'
 
-
-
 export default function Home() {
   const user = useContext(UserContext)
-  if(user?.user.name !== "guest"){
-    return (
-      <>
-        <h1>aaa</h1>
-      </>
-    )
-  } else {
     return (
       <>
         <Banner>
-          <h1>Simplified {user.user.name}</h1>
+          <h1>Simplified {user?.user.name}</h1>
           <button className="confirm"><a href="/register">Join</a></button>
         </Banner>
         <Exemplo/>
       </>
     )
-  }
 }
